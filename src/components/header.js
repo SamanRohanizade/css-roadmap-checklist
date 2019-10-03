c7434d8a54115ac482e531b70d8dcdc61bf73fc2
Saman <saman.rohanizade@gmail.com>
2019-10-03 22:15:04 +0330

    add Fira Sans font

--- src/components/header.js
+++ src/components/header.js
@@ -4,32 +4,6 @@ import React from "react"
 import style from "./header.module.scss"
 
 const Header = ({ siteTitle }) => (
-  // <header
-  //   style={{
-  //     background: `rebeccapurple`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `1.45rem 1.0875rem`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </header>
   <header className={style.header}>
     <h1>{siteTitle}</h1>
   </header>
