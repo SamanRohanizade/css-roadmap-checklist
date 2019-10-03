3d92cc5e3731e58595b4449810b80a122d0d6252
Saman <saman.rohanizade@gmail.com>
2019-10-03 23:07:33 +0330

    change page title and add noindex meta tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CSS Roadmap Checklist`,
+    description: `a roadmap for CSS Mastery`,
+    author: `Saman Rohanizade`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/seo.js
+++ src/components/seo.js
@@ -36,6 +36,10 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
+          name: 'robots',
+          content: 'noindex'
+        },
+        {
           name: `description`,
           content: metaDescription,
         },
@@ -50,23 +54,7 @@ function SEO({ description, lang, meta, title }) {
         {
           property: `og:type`,
           content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
+        }
       ].concat(meta)}
     />
   )
