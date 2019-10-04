29c467512667e95d5f87743601c82027868f7868
Saman <saman.rohanizade@gmail.com>
2019-10-04 12:21:28 +0330

    remove noindex metatag

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,10 +36,6 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
-          name: 'robots',
-          content: 'noindex'
-        },
-        {
           name: `description`,
           content: metaDescription,
         },
