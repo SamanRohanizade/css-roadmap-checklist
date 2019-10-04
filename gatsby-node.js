7f8092a05de343b654babc0c75b50c5cbca05772
Saman <saman.rohanizade@gmail.com>
2019-10-04 11:01:42 +0330

    add new SourceNode for checklistItems inside gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  let checkListItems = [
+    {
+      title: 'Learn the basics',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, impedit!'
+    },
+    {
+      title: 'Learn the advanced stuff',
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit perspiciatis eligendi aliquid.'
+    }
+  ];
+
+  checkListItems.forEach(item => {
+    let checkListItem = {
+      ...item,
+      id: createNodeId('item-' + item.title),
+      internal: {
+        type: 'checkListItem',
+        contentDigest: createContentDigest(item)
+      }
+    };
+
+    actions.createNode(checkListItem);
+  });
+};
