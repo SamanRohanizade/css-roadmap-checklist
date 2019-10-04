d568af69e3300b632a51f3fe37b597cfcc8ea5e3
Saman <saman.rohanizade@gmail.com>
2019-10-04 11:04:22 +0330

    loop through all checklistItems in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,62 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import styles from "./index.module.scss"
+import Layout from "src/components/layout"
+import Card from "src/components/card"
+import SEO from "src/components/seo"
+import { graphql } from 'gatsby'
+import completedImage from 'src/images/success.svg'
+import incomplteImage from 'src/images/incomplete.svg'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const IndexPage = ({ data }) => {
+  let [ completedItems, setcompletedItems ] = useState({});
+  useEffect(() => {
+    setcompletedItems(JSON.parse(localStorage.getItem('completedItems')) || {});
+  }, [])
 
-const IndexPage = () => (
-  <Layout>
+  let checkListItems = data.allCheckListItem.nodes;
+
+  const togglecompletedItems = (id) => {
+    let toggled;
+    if (completedItems[id]) {
+      toggled = { ...completedItems };
+      delete toggled[id];
+    } else {
+      toggled = { ...completedItems, [id]: true };
+    }
+
+    setcompletedItems(toggled);
+    localStorage.setItem('completedItems', JSON.stringify(toggled))
+  }
+
+  return (<Layout>
     <SEO title="Home" />
     
-  </Layout>
-)
+    <div className={styles.checklist}>
+      {checkListItems.map(item => (
+        <div className={styles.checklistItem} key={item.id} onClick={() => togglecompletedItems(item.id)}>
+          <Card
+            title={item.title}
+            text={item.description}
+            ribbonColor={completedItems[item.id] ? 'green' : 'red'}
+            cardIcon={completedItems[item.id] ? completedImage : incomplteImage}
+            cardState={completedItems[item.id] ? 'Complete!' : 'In progress!'}
+          ></Card>
+        </div>
+      ))}
+    </div>
+  </Layout>)
+}
+
+export const query = graphql`
+query {
+  allCheckListItem {
+    nodes {
+      description
+      title
+      id
+    }
+  }
+}
+`
 
 export default IndexPage
